6aed95daa8aa5d56d7045a3483c985e93caf7755
Amanda Wang <ayw23@cornell.edu>
2020-08-21 15:19:24 -0400

    Updated pandas features, finalized logic

--- index.py
+++ index.py
@@ -1,73 +1,51 @@
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
+import csv
+import matplotlib.pyplot as plt
 import warnings
 
-warnings.simplefilter(action='ignore', category=FutureWarning)
+# Define CSV files, replace as needed
+EXT870_dayone = "EXT870_2GU_20200706.CSV"
+EXT871_dayone = "EXT871_2GU_218076_20200706.CSV"
+EXT872_dayone = "EXT872_2GU_218077_20200706.CSV"
 
-# Define CSV file
-EXT870_0706 = "EXT870_2GU_20200706.CSV"
-EXT871_0706 = "EXT871_2GU_218076_20200706.CSV"
-EXT872_0706 = "EXT872_2GU_218077_20200706.CSV"
-
-# Read in CSV file
-df_870 = pd.read_csv(EXT870_0706)
-df_871 = pd.read_csv(EXT871_0706)
-df_872 = pd.read_csv(EXT872_0706)
-
-# Sort CSV files by columns and contents of columns
-df_870_sorted = df_870.loc[
-    (df_870["AccountNumber"] == "2GU05001") & (df_870["AccountType"] == "1")
-]
-df_870_sorted2 = df_870_sorted.loc[
-    :, df_870_sorted.columns.isin(["AccountNumber", "AccountType", "Cusip", "Quantity"])
-]
+EXT870_daytwo = "EXT870_2GU_20200707.CSV"
+EXT871_daytwo = "EXT871_2GU_218076_20200707.CSV"
+EXT872_daytwo = "EXT872_2GU_218077_20200707.CSV"
 
 
-df_871_sorted = df_871.loc[
-    (df_871["AccountNumber"] == "2GU05001") & (df_871["AccountType"] == "1")
-]
-# Renamed columns in 871
-df_871_sorted2 = df_871_sorted.loc[
-    :,
-    df_871_sorted.columns.isin(["AccountNumber", "AccountType", "Cusip", "Quantity"]),
-]
+# Read in CSV file from each date, replace as needed
+df_870_dayone = pd.read_csv(EXT870_dayone)
+df_871_dayone = pd.read_csv(EXT871_dayone)
+df_872_dayone = pd.read_csv(EXT872_dayone)
 
-# Does not account for only AccountType of 1
+df_870_daytwo = pd.read_csv(EXT870_dayone)
+df_871_daytwo = pd.read_csv(EXT871_dayone)
+df_872_daytwo = pd.read_csv(EXT872_dayone)
 
-df_872_sorted = df_872.loc[
-    (df_872["AccountNumber"] == "2GU05001") & (df_872["AccountType"] == 1)
-]
-df_872_sorted2 = df_872_sorted.loc[
-    :, df_872_sorted.columns.isin(["AccountNumber", "AccountType", "Cusip", "Quantity"])
-]
-print (df_871_sorted2)
+# Organize EXT files based on AccountNumber, Account Type, Cusip, Quantity
+# Logic of prediction position = EXT871 of DayOne + EXT 870 of DayTwo + EXT 872 of Day Two  
+# Example: EXT871 of 7/6/2020 + EXT 870 of 7/7/2020 + EXT 872 of 7/7/2020 
+df1 = df_870_daytwo[['AccountNumber', 'AccountType', 'Cusip', 'Quantity']].groupby(['AccountNumber', 'AccountType', 'Cusip']).sum().reset_index()
+df2 = df_871_dayone[['AccountNumber', 'AccountType', 'Cusip', 'Quantity']].groupby(['AccountNumber', 'AccountType', 'Cusip']).sum().reset_index()
+df3 = df_872_daytwo[['AccountNumber', 'AccountType', 'Cusip', 'Quantity']].groupby(['AccountNumber', 'AccountType', 'Cusip']).sum().reset_index()
 
-#df_871_sorted2['Cusip'] = df_872_sorted2['Cusip']
+df3['AccountType'] = df3['AccountType'].astype('str')
 
-#df_872_sorted2['Final Quantity'] = np.where(df_871_sorted2['Cusip'] == df_872_sorted2['Cusip'], 
-#df_871_sorted2['Quantity'] + df_872_sorted2['Quantity'])
-#print (df_871_sorted2)
+#Merge 870 and 871 files and add the Quantity columns
+#IT Suggestion:
+#df_1_2 = pd.merge(df1, df2, on = ['AccountNumber', 'AccountType', 'Cusip'], how ='outer')
+df_1_2 = pd.merge(df1, df2, on = ['AccountNumber', 'AccountType', 'Cusip'])
+df_1_2['Quantity'] = df_1_2['Quantity_x'] + df_1_2['Quantity_y']
+df_1_2 = df_1_2.drop(columns = ['Quantity_x', 'Quantity_y'])
 
-#df_all = pd.concat([df_871_sorted2, df_872_sorted2])
-#pivot = df_all.sort_values("Cusip")
-#print(pivot)
-#df['final'] = 
+#Merge the 872 file, also add the Quantity column
+#IT Suggestion:
+#df_all = pd.merge(df_1_2, df3, on = ['AccountNumber', 'AccountType', 'Cusip'], how ='outer')
+df_all = pd.merge(df_1_2, df3, on = ['AccountNumber', 'AccountType', 'Cusip'])
+df_all['Quantity'] = df_all['Quantity_x'] + df_all['Quantity_y']
+df_all = df_all.drop(columns = ['Quantity_x', 'Quantity_y'])
 
-
-# print(df_third_shift["AccountNumber"])
-
-# df_all = pd.concat([df_first_shift, df_second_shift, df_third_shift])
-# print(df_all)
-
-
-# pivot = df_all.groupby(['Shift']).mean()
-# shift_productivity = pivot.loc[:,"Production Run Time (Min)":"Products Produced (Units)"]
-# loc: choose multiple rows at once, : is every row, and then choose the two columns
-
-# print(shift_productivity)
-
-# #shift_productivity.plot(kind='bar')
-# #plt.show()
-
-# df_all.to_excel("output.xlsx")
+#print the dataframe with the prediction
+print("Listed are the predictions of the second day based on sum of the EXT871 of the first day, EXT870 of the second day, and the EXT 872 of the second day. Entries are organized by the same AccountNumber, AccountType, and Cusip." )
+print(df_all)
